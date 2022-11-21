d75be34f8339a895f5468ac59dea351a2d6efc59
Tobias Mesquita <tobias.mesquita@gmail.com>
2022-11-21 13:41:13 -0300

    first commit

---
+++ src/i18n/en-US/index.ts
@@ -0,0 +1,9 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  text: 'Hello World',
+  interpolateObj: 'My name is {name} {surname}',
+  interpolateArr: 'My name is {0} {1}',
+  plural: '1 potato | {n} potatoes',
+};
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-list style="min-width: 480px">
+      <q-item clickable v-ripple>
+        <q-item-section>text</q-item-section>
+        <q-item-section>{{ t('text') }}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>interpolate Object</q-item-section>
+        <q-item-section>{{ t('interpolateObj', nameA) }}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>interpolate Array</q-item-section>
+        <q-item-section>{{ t('interpolateArr', nameB) }}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>Single</q-item-section>
+        <q-item-section>{{ t('plural', 1) }}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>Plural</q-item-section>
+        <q-item-section>{{ t('plural', { n: number }) }}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>Number</q-item-section>
+        <q-item-section>{{ n(number) }}</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>Date</q-item-section>
+        <q-item-section>{{ d(date) }}</q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t, n, d } = useI18n();
+const nameA = ref({ name: 'Tobias', surname: 'Mesquita' });
+const nameB = ref(['Tobias', 'Mesquita']);
+const number = ref(1234);
+const date = ref(new Date());
+</script>
